e227983ebb41d99d10ae4179ac79cf09b2b2f4c2
Muhammed Arshad <arshadmuhd2001@gmail.com>
2022-12-21 21:31:41 +0530

    skill house session tests

---
+++ session5Test/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter either True or false");
+bool  ans=Convert.ToBoolean(Console.ReadLine());
+
+while (ans == true) 
+{
+    Console.WriteLine("Sorry, you have chosen to move on");
+    Console.WriteLine("enter either True or false");
+    ans = Convert.ToBoolean(Console.ReadLine());
+}
+
+Console.WriteLine("Thank you.You have ended the loop");
