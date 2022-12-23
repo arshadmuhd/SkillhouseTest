8c25bd9164983fc48ea74efd19d18da3a9c3f834
Muhammed Arshad <arshadmuhd2001@gmail.com>
2022-12-23 22:32:49 +0530

    skill house session7 tests

---
+++ session7/Program.cs
@@ -0,0 +1,15 @@
+﻿//session7 oops concept 
+//1.encapsulations
+
+using session7;
+
+
+Student s1=new Student();
+s1.getValues();
+
+
+Student s2 = new Student();
+s2.getValues();
+s1.display();
+s2.display();
+
---
+++ session7/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session7
+{
+    internal class Student  
+    {
+        /*internal Student(string name,int id,string mark)
+        {
+            this.sName = name;
+            this.sId = id;
+            this.sMark = mark;
+        }*/
+
+
+        private string sName { get; set; }
+        private int sId { get; set; }
+        public int sMark { get; set; }
+
+        public void getValues()
+        {
+            Console.WriteLine("Enter student name:");
+             sName=Console.ReadLine();
+            Console.WriteLine("Enter student Id:");
+             sId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter student mark:");
+             sMark = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void collegeName()
+        {
+            Console.WriteLine("SHREE DEVI INSTITUTE");
+        }
+
+       public void display()
+        {
+            Console.WriteLine("Name= {0}\nId= {1}\nMark= {2}",sName,sId,sMark);
+            collegeName();
+        }
+    }
+}
