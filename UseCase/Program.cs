37fe1590ab842caf066d38ae81344ac26cabcd84
Muhammed Arshad <arshadmuhd2001@gmail.com>
2023-02-06 21:18:58 +0530

    Use Case2

---
+++ UseCase/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using UseCase;
+
+ContactInformation contact1 = new ContactInformation
+{
+	Address = "Mangalore,karnataka,India",
+    EmailAddress = "Koylarestuarent@gmail.com",
+	PhoneNumber=9876543210
+
+};
+ContactInformation contact2 = new ContactInformation
+{
+	Address = "Bangalore,karnataka,India",
+	EmailAddress = "Kochinerestuarent@gmail.com",
+	PhoneNumber = 4561237890
+
+};
+
+Cuisine CuisineDetails1 = new Cuisine
+{
+	Type="A/c FastFood",
+	Description="Mangalore best fastfood restuarent"
+
+};
+
+Cuisine CuisineDetails2 = new Cuisine
+{
+	Type = "Non A/c FastFood & Veg",
+	Description = "Bangalore famous Veg and fastfood restuarent"
+
+};
+
+Restaurant R1 = new Restaurant
+{
+	Name="Koyla Restuarent",
+	Location="Mangalore",
+	CuisineDetails=CuisineDetails1,
+	Contact=contact1,
+	AverageCost=5000,
+	NoOfTables=20
+};
+
+Restaurant R2 = new Restaurant
+{
+	Name = "Kochine Restuarent",
+	Location = "Bangalore",
+	CuisineDetails = CuisineDetails2,
+	Contact = contact2,
+	AverageCost = 1000,
+	NoOfTables = 10
+};
+
+R1.DisplayRestaurantInformation();
+Console.WriteLine();
+R2.DisplayRestaurantInformation();
+
+
+
+
+
---
+++ UseCase/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase
+{
+	internal class Restaurant
+	{
+		public string Name { get; set; }	
+		public string Location { get; set; }
+		public Cuisine CuisineDetails { get; set; }
+		public int AverageCost { get; set; }
+		public int NoOfTables { get; set; }
+		public ContactInformation Contact { get; set; }
+
+		public void DisplayRestaurantInformation()
+		{
+			Console.WriteLine("Name:" + Name);
+			Console.WriteLine("Location:" + Name);
+			Console.WriteLine("Cuisine Type:" + CuisineDetails.Type);
+			Console.WriteLine("Cuisine Description:" + CuisineDetails.Description);
+			Console.WriteLine("Address:" + Contact.Address);
+			Console.WriteLine("Email Address:" + Contact.EmailAddress);
+			Console.WriteLine("Phone Number:" + Contact.PhoneNumber);
+			Console.WriteLine("Average Cost:" + AverageCost);
+			Console.WriteLine("No Of Tables:" + NoOfTables);
+		}
+
+	}
+}
