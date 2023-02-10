b7196542e780183f76034436a64b05d6bdb2d6ee
Muhammed Arshad <arshadmuhd2001@gmail.com>
2023-02-10 21:18:22 +0530

    Updated Use Case4 Inheritance and polymorphism

--- UseCase4Polimorphism/SecurityPerson.cs
+++ UseCase4Polimorphism/SecurityPerson.cs
@@ -8,11 +8,9 @@ namespace UseCase4
 {
     internal class SecurityPerson:Employee
     {
-        public override void Walk()
-        {
-            Console.WriteLine("Hi, there. My name is {0}. I am walking", Name);
+        public ShiftTime Shift { get; set; }
 
-        }
+
         public override void Work()
         {
             Console.WriteLine("Hi, there. I am working as security.");
